92998159ecfad81a142d527a052980b55ab57acc
Carter Bush <carter.l.bush@gmail.com>
2023-05-20 23:55:29 +1000

    Add header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,23 @@ import CharacterList from '../components/CharacterList.vue'
 
 <template>
   <main>
+    <header><h1>In and out, 15 minute Vue adventure</h1></header>
     <CharacterList />
   </main>
 </template>
+
+<style>
+header {
+  text-align: center;
+  font-family: 'Get Schwifty';
+  -webkit-text-stroke: 1px #fff765;
+  color: #08bae3;
+  font-size: 32px;
+}
+
+@media (max-width: 700px) {
+  header {
+    font-size: 16px;
+  }
+}
+</style>
