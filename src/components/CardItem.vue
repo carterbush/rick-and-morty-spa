5504b3d156723bfdb57ff138f863b915192d1768
Carter Bush <carter.l.bush@gmail.com>
2023-05-20 23:28:17 +1000

    Add a card item component

---
+++ src/components/CardItem.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+export default {
+  props: {
+    title: String
+  }
+}
+</script>
+
+<template>
+  <div class="card-item">
+    <div>
+      <slot></slot>
+    </div>
+    <div class="card-title">
+      <h4>{{ title }}</h4>
+    </div>
+  </div>
+</template>
+
+<style>
+.card-item {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+}
+
+.card-title {
+  text-align: center;
+}
+</style>
