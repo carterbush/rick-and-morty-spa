e09b5fb1b9e7342728d5e20dd1c367c8c62c8c51
Carter Bush <carter.l.bush@gmail.com>
2023-05-25 21:20:58 +1000

    Change header styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,23 +4,42 @@ import CharacterList from '../components/CharacterList.vue'
 
 <template>
   <main>
-    <header><h1>In and out, 15 minute Vue adventure</h1></header>
+    <header>
+      <h1>In and out</h1>
+      <h1>20 minute Vue adventure</h1>
+    </header>
     <CharacterList />
   </main>
 </template>
 
-<style>
+<style lang="scss">
 header {
-  text-align: center;
-  font-family: 'Get Schwifty';
-  -webkit-text-stroke: 1px #fff765;
-  color: #08bae3;
-  font-size: 32px;
-}
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
 
-@media (max-width: 700px) {
-  header {
-    font-size: 16px;
+  h1 {
+    color: #08bae3;
+
+    font-size: 32px;
+    font-family: 'Get Schwifty';
+    white-space: nowrap;
+
+    text-align: center;
+    text-shadow: 0 0 16px #fff765;
+    -webkit-text-stroke: 1px black;
+
+    &:last-child {
+      margin-left: 0.25em;
+    }
+  }
+
+  @media (min-width: 800px) {
+    flex-direction: row;
+
+    h1 {
+      font-size: 48px;
+    }
   }
 }
 </style>
